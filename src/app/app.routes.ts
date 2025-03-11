6c9f6cc8013d4844f15d8d9001f17a8e126b04b2
mohamed-Elhossin <m.m.m.elhossin@gmail.com>
2025-03-11 22:26:28 +0200

    add

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/components/home/home.component';
+import { AboutComponent } from './features/components/about/about.component';
+import { CoursesComponent } from './features/components/courses/courses.component';
+import { PricingComponent } from './features/components/pricing/pricing.component';
+import { TrainersComponent } from './features/components/trainers/trainers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"home" , component : HomeComponent},
+  {path:"about" , component : AboutComponent},
+  {path:"courses" , component : CoursesComponent},
+  {path:"pricing" , component : PricingComponent},
+  {path:"trainers" , component : TrainersComponent},
+];
